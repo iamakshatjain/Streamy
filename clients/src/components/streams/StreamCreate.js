f55bcbe49a25e46b54b1c9fbd5f7d5a8da928dbc
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-07-05 02:33:01 +0530

    setup streamCreate form

--- clients/src/components/streams/StreamCreate.js
+++ clients/src/components/streams/StreamCreate.js
@@ -1,9 +1,55 @@
 import React from 'react'
 
+import {Field, reduxForm} from 'redux-form';
+
 class StreamCreate extends React.Component{
+
+	renderError({touched,error}){
+		if(touched && error){
+			return(
+				<div className="ui error message" >
+					<div className="header">{error}</div>
+				</div>
+				);
+		}
+	}
+
+	renderInput = ({input,label,meta}) => {
+		const className = `field ${meta.touched && meta.error ? 'error' : ''}`;
+		return(
+				<div className={className}>
+					<label>{label}</label>
+					<input {...input} autoComplete="off"/>
+					{this.renderError(meta)}
+				</div>
+			); 
+	}
+
+	onSubmit(formValues){
+		// console.log(formValues);
+	}
+
 	render(){
-		return <div>Stream Create</div>
+		return (
+				<form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)} >
+					<Field name="title" component={this.renderInput} label="Title"/>
+					<Field name="description" component={this.renderInput} label="Description"/>
+					<button className="ui button primary">Submit</button>
+				</form>
+			);
 	}
 }
 
-export default StreamCreate;
+const validate = formValues => {
+	let errors = {};
+
+	if(!formValues.title)
+		errors.title = "Enter a title!";
+	if(!formValues.description)
+		errors.description = "Enter a description";
+
+	return errors;
+}
+
+export default reduxForm({form : 'StreamCreate',validate:validate})(StreamCreate);
+
--- clients/src/reducers/index.js
+++ clients/src/reducers/index.js
@@ -1,8 +1,10 @@
 // import React from 'react';
 import {combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
 
 import AuthReducer from './AuthReducer';
 
 export default combineReducers({
-	auth : AuthReducer //here we pass the reference to the function
+	auth : AuthReducer, //here we pass the reference to the function
+	form : formReducer
 });
